86c83a132de6c7e28fab600d4138c07e4a89589d
Felipe Matheus Flohr <felipeflohrlol@gmail.com>
2022-08-17 22:54:34 -0300

    Remodelado jogo

--- src/views/slides/13/Thirteenth.tsx
+++ src/views/slides/13/Thirteenth.tsx
@@ -5,16 +5,18 @@ import path from "path";
 export default function Thirteenth() {
     const { unityProvider } = useUnityContext({
         loaderUrl: path.resolve("src/assets/game/Build/BuildFPS.loader.js"),
-        dataUrl: path.resolve("src/assets/game/Build/BuildFPS.data.gz"),
-        codeUrl: path.resolve("src/assets/game/Build/BuildFPS.wasm.gz"),
-        frameworkUrl: path.resolve("src/assets/game/Build/BuildFPS.framework.js.gz")
+        dataUrl: path.resolve("src/assets/game/Build/BuildFPS.data"),
+        codeUrl: path.resolve("src/assets/game/Build/BuildFPS.wasm"),
+        frameworkUrl: path.resolve("src/assets/game/Build/BuildFPS.framework.js")
     })
     return (
         <CenterTitleSlide title="WebAssembly">
-            <Unity unityProvider={unityProvider} style={{
-                width: "800px",
-                height: "600px"
-            }} />
+            <div className="flex flex-col w-full h-full items-center justify-center">
+                <Unity unityProvider={unityProvider} style={{
+                    width: "800px",
+                    height: "600px"
+                }} />
+            </div>
         </CenterTitleSlide>
     )
 }
